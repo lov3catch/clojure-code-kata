be89c3e576223ec9997d1f68d657584bb20bca17
lov3catch <lov3catch@gmail.com>
2020-02-17 11:09:20 +0200

    upload

---
+++ test/clojure_code_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-code-kata.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-code-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
